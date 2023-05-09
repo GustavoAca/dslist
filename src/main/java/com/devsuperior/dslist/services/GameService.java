f16a96960433a0ab1e1f7ac58cd56f0caf16c13e
gustavo.acacio <gustavo.acacio@attornatus.com.br>
2023-05-09 20:41:52 -0300

    finalizacao aula 1

--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -1,7 +1,6 @@
 package com.devsuperior.dslist.services;
 
 import com.devsuperior.dslist.dto.GameMinDTO;
-import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,7 +15,6 @@ public class GameService {
     public GameService(GameRepository gameRepository) {
         this.gameRepository = gameRepository;
     }
-
 
     public List<GameMinDTO> findAll() {
        var games =  gameRepository.findAll();
