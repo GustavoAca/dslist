4d5f3f132940e174d9de4300d17692dd7f583e98
gustavo.acacio <gustavo.acacio@attornatus.com.br>
2023-05-10 22:17:20 -0300

    ajustes ausentes em aula 2

--- src/main/java/com/devsuperior/dslist/controller/GameListController.java
+++ src/main/java/com/devsuperior/dslist/controller/GameListController.java
@@ -1,7 +1,9 @@
 package com.devsuperior.dslist.controller;
 
 import com.devsuperior.dslist.dto.GameListDto;
+import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.services.GameListService;
+import com.devsuperior.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,10 +16,13 @@ import java.util.List;
 @RequestMapping(value = "/lists")
 public class GameListController {
     private final GameListService gameListService;
+    private final GameService gameService;
 
     @Autowired
-    public GameListController(GameListService gameListService) {
+    public GameListController(GameListService gameListService,
+                              GameService gameService) {
         this.gameListService = gameListService;
+        this.gameService = gameService;
     }
 
     @GetMapping
@@ -28,5 +33,10 @@ public class GameListController {
     @GetMapping(value = "/{id}")
     public GameListDto findById(@PathVariable Long id) {
         return gameListService.findById(id);
+    }
+
+    @GetMapping(value = "/{listId}/games")
+    public List<GameMinDTO> findByList(@PathVariable Long listId) {
+        return gameService.findByList(listId);
     }
 }
