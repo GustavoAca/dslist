26b7f6405046e5f7b1ce7e1dcaba32f6135e07ed
gustavo.acacio <gustavo.acacio@attornatus.com.br>
2023-05-11 22:10:12 -0300

    move position

--- src/main/java/com/devsuperior/dslist/services/GameListService.java
+++ src/main/java/com/devsuperior/dslist/services/GameListService.java
@@ -2,7 +2,9 @@ package com.devsuperior.dslist.services;
 
 import com.devsuperior.dslist.dto.GameListDto;
 import com.devsuperior.dslist.exception.NaoEncontradoException;
+import com.devsuperior.dslist.projections.GameMinProjection;
 import com.devsuperior.dslist.repositories.GameListRepository;
+import com.devsuperior.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,21 +13,36 @@ import java.util.List;
 
 @Service
 public class GameListService {
-    private final GameListRepository gameRepository;
+    private final GameListRepository gameListRepository;
+    private final GameRepository gameRepository;
 
     @Autowired
-    public GameListService(GameListRepository gameRepository) {
+    public GameListService(GameListRepository gameListRepository, GameRepository gameRepository) {
+        this.gameListRepository = gameListRepository;
         this.gameRepository = gameRepository;
     }
 
     @Transactional(readOnly = true)
     public List<GameListDto> findAll() {
-        return gameRepository.findAll().stream().map(GameListDto::new).toList();
+        return gameListRepository.findAll().stream().map(GameListDto::new).toList();
+    }
+
+    @Transactional
+    public void move(Long listId, int sourceIndex, int destinationIndex) {
+        List<GameMinProjection> list = gameRepository.searchByList(listId);
+        GameMinProjection obj = list.remove(sourceIndex);
+        list.add(destinationIndex, obj);
+        int posicaoMin = Math.min(sourceIndex, destinationIndex);
+        int posicaoMax = Math.max(sourceIndex, destinationIndex);
+
+        for (int i = posicaoMin; i <= posicaoMax; i++) {
+            gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+        }
     }
 
     @Transactional(readOnly = true)
     public GameListDto findById(Long id) {
-        return gameRepository.findById(id).stream().map(GameListDto::new).findFirst().orElseThrow(() -> new NaoEncontradoException("Id não encontrado"));
+        return gameListRepository.findById(id).stream().map(GameListDto::new).findFirst().orElseThrow(() -> new NaoEncontradoException("Id não encontrado"));
     }
 
 }
